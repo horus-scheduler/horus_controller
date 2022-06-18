d45f7c98539eb202b2997436b3d09ae86242d7aa
Khaled Diab <kdiab@sfu.ca>
2022-06-17 23:14:42 -0700

    :hammer: move the messages between RPC<->object model to a separate file

---
+++ core/net/messages.go
@@ -0,0 +1,59 @@
+package net
+
+import (
+	"github.com/khaledmdiab/horus_controller/core/model"
+	horus_pb "github.com/khaledmdiab/horus_controller/protobuf"
+)
+
+type VCUpdateType uint32
+
+const (
+	VCUpdateAdd VCUpdateType = iota
+	VCUpdateRem
+)
+
+type LeafFailedMessage struct {
+	Leaf *horus_pb.LeafInfo
+	Dsts []*model.Node
+}
+
+type ServerFailedMessage struct {
+	Server *horus_pb.ServerInfo
+	Dsts   []*model.Node
+}
+
+type ServerAddedMessage struct {
+	Server *horus_pb.ServerInfo
+	Dst    *model.Node
+}
+
+type LeafAddedMessage struct {
+	Leaf *horus_pb.LeafInfo
+	Dst  *model.Node
+}
+
+type VCUpdatedMessage struct {
+	VCInfo *horus_pb.VCInfo
+	Dsts   []*model.Node
+	Type   VCUpdateType
+}
+
+func NewLeafFailedMessage(leaf *horus_pb.LeafInfo, dsts []*model.Node) *LeafFailedMessage {
+	return &LeafFailedMessage{Leaf: leaf, Dsts: dsts}
+}
+
+func NewServerFailedMessage(server *horus_pb.ServerInfo, dsts []*model.Node) *ServerFailedMessage {
+	return &ServerFailedMessage{Server: server, Dsts: dsts}
+}
+
+func NewServerAddedMessage(server *horus_pb.ServerInfo, dst *model.Node) *ServerAddedMessage {
+	return &ServerAddedMessage{Server: server, Dst: dst}
+}
+
+func NewLeafAddedMessage(leaf *horus_pb.LeafInfo, dst *model.Node) *LeafAddedMessage {
+	return &LeafAddedMessage{Leaf: leaf, Dst: dst}
+}
+
+func NewVCUpdatedMessage(vcInfo *horus_pb.VCInfo, vcType VCUpdateType, dsts []*model.Node) *VCUpdatedMessage {
+	return &VCUpdatedMessage{VCInfo: vcInfo, Type: vcType, Dsts: dsts}
+}
